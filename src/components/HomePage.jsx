5a00a3acb6c92932808ed0c0e5a8b5ead8d1db15
Aquilaruspante <calogero.salvaggio3@gmail.com>
2025-11-13 22:14:02 +0000

    Add button interactivity

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,12 +2,12 @@ import styles from './homePage.module.css';
 
 export default function HomePage() {
     return (
-        <>
+        <div className={styles.manropeFont}>
             <p className={styles.hero}>We guaratee the best prices on the market</p>
             <p className={styles.hero}>If you find it cheaper we reimburse you</p>
             <div className={styles.buttonContainer}>
                 <button className={styles.btn}>Shop with us</button>
             </div>
-        </>
+        </div>
     )
 }
