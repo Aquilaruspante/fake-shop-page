a40ab09f0549d9fbc22025413c8d59fbf84a96a5
Aquilaruspante <calogero.salvaggio3@gmail.com>
2025-11-15 16:44:29 +0000

    Add store menu

--- src/components/Store/Card.jsx
+++ src/components/Store/Card.jsx
@@ -1,12 +1,18 @@
 import styles from './Card.module.css';
+import { useFetcher } from 'react-router';
+import { ShoppingCart } from 'lucide-react';
 
 export default function Card({ title, price, description, category, image }) {
+    const fetcher = useFetcher();
+
     return(
         <div data-testid='card' className={styles.card}>
             <img src={image} alt={title} />
-            <h2>{title}</h2>
-            <p>{price} $</p>
-            <button className={styles.btn}>Add to cart</button>
+            <h2 className={styles.montserratRegular}>{title.slice(0, 40)}...</h2>
+            <div className={styles.lastLine}>
+                <p className={styles.price}>{price} $</p>
+                <button className={styles.btn}>+<ShoppingCart size={15} strokeWidth={3}/></button>
+            </div>           
         </div>
     )
 }
--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,10 +1,11 @@
 import styles from './Store.module.css';
-import { useLoaderData, Form, useNavigation } from "react-router";
+import { useLoaderData, Form, useNavigation, NavLink } from "react-router";
 import Card from "./Card";
 
 export async function loader() {
     const response = await fetch('https://fakestoreapi.com/products');
     const data = await response.json();
+    console.log(data);
     return data;
 } 
 
@@ -16,9 +17,15 @@ export default function Store() {
 
     return(
         <>
-            <Form>
-                <input type="search" />
-            </Form>
+            <div className={styles.secondHeader}>
+                <NavLink className={styles.a}>Men's clothing</NavLink>
+                <NavLink className={styles.a}>Women's clothing</NavLink>
+                <NavLink className={styles.a}>Jewelery</NavLink>
+                <NavLink className={styles.a}>Electronics</NavLink>
+                 <Form className={styles.form}>
+                    <input type="search" placeholder='Search Item...'/>
+                </Form>
+            </div>
             <ul className={styles.container}>
                 {data.map((item) => (
                     <li key={item.id}><Card title={item.title} price={item.price} description={item.description} category={item.category} image={item.image} /></li>
