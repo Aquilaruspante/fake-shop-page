e8f06452e6e2ed19dc9989265db26f0f81bdd787
Aquilaruspante <calogero.salvaggio3@gmail.com>
2025-11-20 16:42:38 +0000

    link underlined when active

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -5,11 +5,12 @@ export default function Store() {
     return(
         <>
             <nav className={styles.secondHeader}>
-                <NavLink className={styles.a} to={'/store'}>All</NavLink>
-                <NavLink className={styles.a} to={'/store/mensclothing'}>Men's clothing</NavLink>
-                <NavLink className={styles.a} to={'/store/womensclothing'}>Women's clothing</NavLink>
-                <NavLink className={styles.a} to={'/store/jewelery'}>Jewelery</NavLink>
-                <NavLink className={styles.a} to={'/store/electronics'}>Electronics</NavLink>
+                <NavLink 
+                    className={({ isActive }) => isActive ? `active ${styles.a}` : styles.a} to={'/store'}>All</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/mensclothing'}>Men's clothing</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/womensclothing'}>Women's clothing</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/jewelery'}>Jewelery</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/electronics'}>Electronics</NavLink>
                 <Form 
                     className={styles.form}
                     role="search">
