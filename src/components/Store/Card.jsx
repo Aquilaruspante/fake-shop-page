36820ecde549a2419ef7d5c8f091e17d2758a7bd
Aquilaruspante <calogero.salvaggio3@gmail.com>
2025-11-30 23:25:28 +0000

    Add link ti ProductPage on store img and titles

--- src/components/Store/Card.jsx
+++ src/components/Store/Card.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router';
 import styles from './Card.module.css';
 import AddToCartButton from './addToCartButton';
 
 export default function Card({ item, cart }) {
+    const navigate = useNavigate();
 
     return(
         <div data-testid='card' className={styles.card}>
-            <img src={item.image} alt={item.title} />
-            <h2 data-testid='card-title' className={styles.montserratRegular}>{item.title.slice(0, 40)}...</h2>
+            <img src={item.image} alt={item.title} onClick={() => navigate(`/store/products/${item.id}`)} />
+            <h2 data-testid='card-title' className={styles.montserratRegular} onClick={() => navigate(`/store/products/${item.id}`)}>{item.title.slice(0, 40)}...</h2>
             <div className={styles.lastLine}>
                 <p aria-label='price' className={styles.price}>{item.price} $</p>
                 <div className={styles.btn}>
