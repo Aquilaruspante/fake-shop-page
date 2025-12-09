a8397bb9b33c892aabeb4a37f26936d1b97f0dc1
Aquilaruspante <calogero.salvaggio3@gmail.com>
2025-12-09 21:08:41 +0000

    add link to cartCard img and title

--- src/components/Cart/CartCard.jsx
+++ src/components/Cart/CartCard.jsx
@@ -1,15 +1,18 @@
 import styles from './CartCard.module.css';
 import AddToCartButton from '../Store/addToCartButton';
 import cardStyles from '../Store/Card.module.css';
+import { useNavigate } from 'react-router';
 
 export default function CartCard({ item, cart }) {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.productCard} aria-label="product card">
             <div className={styles.leftSide}>
-                <img className={styles.image} src={item.image} alt="item image" />
+                <img className={styles.image} src={item.image} alt="item image" onClick={() => navigate(`/store/products/${item.id}`)}/>
             </div>
             <div className={styles.rightSide}>
-                <h2 aria-label="title" className={styles.cardTitle}>{item.title}</h2>
+                <h2 aria-label="title" className={styles.cardTitle} onClick={() => navigate(`/store/products/${item.id}`)}>{item.title}</h2>
                 <p aria-label='price' className={styles.cardPrice}>Price: {item.price} $</p>
                 <div className={styles.lastLine}>
                     <div className={styles.quantity}>Quantity: {item.quantity}</div>
