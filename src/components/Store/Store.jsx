5cc36250f0b20ba0c0e5cc896e7abc7429a115f3
Aquilaruspante <calogero.salvaggio3@gmail.com>
2025-12-12 23:06:04 +0000

    remove onClick from Navlinks in Store

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -23,8 +23,8 @@ export default function Store() {
         const form = document.querySelector('form[role="search"]');
         if (!form) return;
 
-        const timeOutId = setTimeout(() => {
-                submit(form, { replace: true });
+        const timeOutId = setTimeout(() => {    
+            submit(form, { replace: true });
         }, 150);
 
         return () => clearTimeout(timeOutId);
@@ -35,11 +35,11 @@ export default function Store() {
     return(
         <>
             <nav className={styles.secondHeader}>
-                <NavLink className={({ isActive }) => isActive ? `active ${styles.a}` : styles.a} to={'/store'} onClick={() => setSearchInput('')}>All</NavLink>
-                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/mensclothing'} onClick={() => setSearchInput('')}>Men's clothing</NavLink>
-                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/womensclothing'} onClick={() => setSearchInput('')}>Women's clothing</NavLink>
-                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/jewelery'} onClick={() => setSearchInput('')}>Jewelery</NavLink>
-                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/electronics'} onClick={() => setSearchInput('')}>Electronics</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `active ${styles.a}` : styles.a} to={'/store'} >All</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/mensclothing'} >Men's clothing</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/womensclothing'} >Women's clothing</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/jewelery'} >Jewelery</NavLink>
+                <NavLink className={({ isActive }) => isActive ? `${styles.active} ${styles.a}` : styles.a} to={'/store/electronics'} >Electronics</NavLink>
                 <Form 
                     className={styles.form}
                     role="search"
